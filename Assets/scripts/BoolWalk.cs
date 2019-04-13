a3933dde418da2ceea31a829d39a80c5d8f1726a
habeeb22 <habeeb22@users.noreply.github.com>
2019-04-13 19:09:20 +0400

    test2

---
+++ Assets/scripts/BoolWalk.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoolWalk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other1)
+    {
+        if (other1.tag == "Moves")
+        {
+            // move = false;
+            PlayerPrefs.SetInt("moving", 0);
+        }
+        else
+        {
+            // move = true;
+            PlayerPrefs.SetInt("moving", 1);
+        }
+    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Moves")
+    //    {
+    //        // move = false;
+    //        PlayerPrefs.SetInt("moving", 0);
+    //    }
+    //    else 
+    //    {
+    //        // move = true;
+    //        PlayerPrefs.SetInt("moving", 1);
+    //    }
+    //}
+}
---
+++ Assets/scripts/MovePos.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePos : MonoBehaviour
+{
+    public bool move = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerPrefs.GetInt("moving", 0) == 1)
+        {
+            print("moving");
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
+        }
+        else 
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
+        
+    }
+    
+}
