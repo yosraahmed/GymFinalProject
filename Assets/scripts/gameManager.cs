c3afe7945ce04603c4e89691b4eed79e95f28243
habeeb22 <habeeb22@users.noreply.github.com>
2019-04-14 21:22:26 +0400

    squatstest2

--- Assets/scripts/gameManager.cs
+++ Assets/scripts/gameManager.cs
@@ -7,6 +7,9 @@ public class GameManager : MonoBehaviour
 {
     public Transform SquatsPoint1;
     public GameObject Squat1;
+    public Transform SquatsPoint2;
+    public GameObject Squat2;
+    public Transform SquatsPoint3;
     public GameObject Bullet1;
     public GameObject Bullet2;
     public GameObject Bullet3;
@@ -42,7 +45,7 @@ public class GameManager : MonoBehaviour
                 Random.Range(transform.position.y + Hight, transform.position.y),
                 transform.position.z);
                 
-            int Bullettype = Random.Range(1, 5);
+            int Bullettype = Random.Range(1, 7);
             if (Bullettype == 1)
             {
                 Instantiate(Bullet1, position, transform.rotation);
@@ -53,11 +56,19 @@ public class GameManager : MonoBehaviour
             }
             else if (Bullettype == 3)
             {
-                Instantiate(Squat1, SquatsPoint1.transform.position, SquatsPoint1.transform.rotation);
+                Instantiate(Bullet3, position, transform.rotation);
             }
             else if (Bullettype == 4)
             {
-                Instantiate(Bullet3, position, transform.rotation);
+                Instantiate(Squat1, SquatsPoint1.transform.position, SquatsPoint1.transform.rotation);
+            }
+            else if (Bullettype == 5)
+            {
+                Instantiate(Squat2, SquatsPoint2.transform.position, SquatsPoint2.transform.rotation);
+            }
+            else if (Bullettype == 6)
+            {
+                Instantiate(Squat1, SquatsPoint3.transform.position, SquatsPoint3.transform.rotation);
             }
 
 
