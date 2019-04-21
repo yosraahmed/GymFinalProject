4194aedad3129e6e58f7e2c6224a26a935524c25
habeeb22 <habeeb22@users.noreply.github.com>
2019-04-21 11:19:07 +0400

    score fixed

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -25,39 +25,42 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PlayerPrefs.SetInt("scorText", bluHit + redHit + colorHit);
+        //PlayerPrefs.SetInt("scorText", bluHit + redHit + colorHit);
         ScourText.text = PlayerPrefs.GetInt("scorText", 0).ToString();
     }
     private void OnTriggerEnter(Collider other)
     {
-        //if (other.tag== "BlueHit")
-        //{
-        //    soundManager.instance.CubeCrashSound();
-        //    bluHit += 1;
-        //    Instantiate(particleBlue, other.transform.position, other.transform.rotation);
-        //    Destroy(other.gameObject);
-            
+        if (other.tag == "BlueHit")
+        {
+            soundManager.instance.CubeCrashSound();
+            bluHit += 1;
+            Instantiate(particleBlue, other.transform.position, other.transform.rotation);
+            Destroy(other.gameObject);
 
-        //}
+
+        }
         if (other.tag == "RedHit")
         {
             soundManager.instance.CubeCrashSound();
-            redHit += 1;
+            PlayerPrefs.SetInt("scorText", PlayerPrefs.GetInt("scorText", 0)+1);
+            //redHit += 1;
             Instantiate(particleRed, other.transform.position, other.transform.rotation);
             Destroy(other.gameObject);
         }
         if (other.tag == "ColorHit")
         {
             soundManager.instance.CubeCrashSound();
-            colorHit += 1;
+            PlayerPrefs.SetInt("scorText", PlayerPrefs.GetInt("scorText", 0) + 1);
+            //colorHit += 1;
             Instantiate(particleColor, other.transform.position, other.transform.rotation);
             Destroy(other.gameObject);
         }
         if (other.tag == "Squat1Hit")
         {
             soundManager.instance.CubeCrashSound();
-            colorHit -= 1;
-            
+            PlayerPrefs.SetInt("scorText", PlayerPrefs.GetInt("scorText", 0) - 1);
+            //colorHit -= 1;
+
             Destroy(other.gameObject);
         }
 
