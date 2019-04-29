8ae55023b60a6bb6bd351e79d7ccb36d2175cfdc
habeeb22 <habeeb22@users.noreply.github.com>
2019-04-29 09:59:46 +0400

    Pause test

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            if (!pausePanel.activeInHierarchy)
+            {
+                PauseGame();
+            }
+            else if (pausePanel.activeInHierarchy)
+            {
+                ContinueGame();
+            }
+        }
+    }
+    private void PauseGame()
+    {
+        PlayerPrefs.SetInt("Timer", 1);
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+        //Disable scripts that still work while timescale is set to 0
+
+    }
+    private void ContinueGame()
+    {
+        PlayerPrefs.SetInt("Timer", 0);
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        //enable the scripts again
+    }
+    public void ExitButton()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenuTestWithOculus");
+    }
+    public void ResumeButton()
+    {
+        ContinueGame();
+    }
+}
