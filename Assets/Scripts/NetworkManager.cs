e3fe26e164ec2580b51dcbb52eea4803b2605b4e
habeeb22 <habeeb22@users.noreply.github.com>
2019-04-30 10:37:41 +0400

    multiplayer vive test

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using TMPro;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    #region Vars
+    string gameVersion = "1";
+    const int MAXPLAYERCOUNT = 2;
+    #endregion
+
+    #region Properties
+    public static NetworkManager Instance { get; set; }
+
+    public bool ConnectedToMaster { get; set; }
+    #endregion
+
+    #region Methods
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.GameVersion = gameVersion;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+       
+        ConnectedToMaster = true;
+    }
+
+    public void Disconnect()
+    {
+        PhotonNetwork.Disconnect();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+    }
+
+    public void JoinRoom()
+    {
+        if (PhotonNetwork.IsConnectedAndReady)
+            PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = MAXPLAYERCOUNT }, null);
+
+        base.OnJoinRandomFailed(returnCode, message);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        print("Joined");
+
+        if (PhotonNetwork.CurrentRoom.PlayerCount >= MAXPLAYERCOUNT)
+            Invoke("StartGame", 3);
+    }
+
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+    {
+        print("Player Joined");
+
+        if (PhotonNetwork.CurrentRoom.PlayerCount >= MAXPLAYERCOUNT)
+            Invoke("StartGame", 3);
+
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+
+    void StartGame()
+    {
+        print("playing");
+        PhotonNetwork.LoadLevel("gameScene2Multiplayer");
+        //Start Game
+    }
+    #endregion
+}
