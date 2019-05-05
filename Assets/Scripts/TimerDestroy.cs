ae7118fb8b21f8fe0c69f8f7cfdc06fa7fe922f2
alafani <48641645+alafani@users.noreply.github.com>
2019-05-05 15:11:45 +0400

    done update number1

--- Assets/Scripts/TimerDestroy.cs
+++ Assets/Scripts/TimerDestroy.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerDestroy : MonoBehaviour
 {
-    float timer = 5;
+    //float timer = 5;
+    float timer = RespawnBoxing.instance.TimeDestroy;
     // Start is called before the first frame update
     void Start()
     {
