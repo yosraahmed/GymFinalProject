1e23826a3b402cf85ceb61d14f47f695f5954d7d
habeeb22 <habeeb22@users.noreply.github.com>
2019-05-13 10:06:13 +0400

    test

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -65,36 +65,36 @@ public class gameManager : MonoBehaviour
             int Bullettype = Random.Range(1, 7);
             if (Bullettype == 1)
             {
-                PhotonNetwork.Instantiate(this.Bullet1.name, position, Quaternion.identity, 0);
+                PhotonNetwork.Instantiate(this.Bullet1.name, position, transform.rotation, 0);
                 //Instantiate(Bullet1, position, transform.rotation);
             }
             else if (Bullettype == 2)
             {
-                PhotonNetwork.Instantiate(this.Bullet2.name, position, Quaternion.identity, 0);
+                PhotonNetwork.Instantiate(this.Bullet2.name, position, transform.rotation, 0);
                 //Instantiate(Bullet2, position, transform.rotation);
             }
             else if (Bullettype == 3)
             {
-                PhotonNetwork.Instantiate(this.Bullet3.name, position, Quaternion.identity, 0);
+                PhotonNetwork.Instantiate(this.Bullet3.name, position, transform.rotation, 0);
                 //Instantiate(Bullet3, position, transform.rotation);
             }
             else if (Bullettype == 4)
             {
-                PhotonNetwork.Instantiate(this.Squat1.name, SquatsPoint1.transform.position, Quaternion.identity, 0);
+                PhotonNetwork.Instantiate(this.Squat1.name, SquatsPoint1.transform.position, SquatsPoint1.transform.rotation, 0);
                 PhotonNetwork.Instantiate(this.Bullet2.name, SquatsPoint11.transform.position, Quaternion.identity, 0);
                 //Instantiate(Squat1, SquatsPoint1.transform.position, SquatsPoint1.transform.rotation);
                 //Instantiate(Bullet2, SquatsPoint11.transform.position, SquatsPoint11.transform.rotation);
             }
             else if (Bullettype == 5)
             {
-                PhotonNetwork.Instantiate(this.Squat2.name, SquatsPoint2.transform.position, Quaternion.identity, 0);
+                PhotonNetwork.Instantiate(this.Squat2.name, SquatsPoint2.transform.position, SquatsPoint2.transform.rotation, 0);
                 PhotonNetwork.Instantiate(this.Bullet3.name, SquatsPoint22.transform.position, Quaternion.identity, 0);
                 //Instantiate(Squat2, SquatsPoint2.transform.position, SquatsPoint2.transform.rotation);
                 //Instantiate(Bullet3, SquatsPoint22.transform.position, SquatsPoint22.transform.rotation);
             }
             else if (Bullettype == 6)
             {
-                PhotonNetwork.Instantiate(this.Squat1.name, SquatsPoint3.transform.position, Quaternion.identity, 0);
+                PhotonNetwork.Instantiate(this.Squat1.name, SquatsPoint3.transform.position, SquatsPoint3.transform.rotation, 0);
                 PhotonNetwork.Instantiate(this.Bullet1.name, SquatsPoint33.transform.position, Quaternion.identity, 0);
                 //Instantiate(Squat1, SquatsPoint3.transform.position, SquatsPoint3.transform.rotation);
                 //Instantiate(Bullet1, SquatsPoint33.transform.position, SquatsPoint33.transform.rotation);
