b8ea316ad062858443604ab8f1fc3950ad57fa61
habeeb22 <habeeb22@users.noreply.github.com>
2019-05-15 12:54:43 +0400

    Boxing Fix

---
+++ Assets/BoxingTestBack.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxingTestBack : MonoBehaviour
+{
+    public bool activeAttachment;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+         //rb.constraints = RigidbodyConstraints.FreezeAll;
+
+        if (activeAttachment == true)
+        {
+            rb.constraints &= ~RigidbodyConstraints.FreezePositionZ;
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -1 * Time.deltaTime);
+            //Vector3 move = new Vector3(0, 0, -1) * Time.deltaTime;
+            //rb.MovePosition(move);
+            //rb.MovePosition(transform.position - transform.forward * Time.deltaTime);
+
+            
+        }
+        else
+        {
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+    private void OnTriggerEnter(Collider other1)
+    {
+        if (other1.tag == "PunchingBag")
+        {
+           
+            activeAttachment = true;
+
+        }
+
+    }
+    private void OnTriggerExit(Collider other2)
+    {
+        if (other2.tag == "PunchingBag")
+        {
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            //rb.constraints = RigidbodyConstraints.FreezeRotationZ;
+            activeAttachment = false;
+        }
+    }
+}
---
+++ Assets/Scripts/glovesAttach.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glovesAttach : MonoBehaviour
+{
+    public GameObject gloves;
+    public Transform glovesLocation;
+    public bool activeAttachment;
+    public static glovesAttach Instanse;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool activeAttachment=true;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+
+        if (activeAttachment == true)
+        {
+            gloves.transform.parent = glovesLocation.transform;
+            gloves.transform.localPosition = Vector3.zero;
+            gloves.transform.localRotation = Quaternion.identity;
+        }
+        else
+        {
+            gloves.transform.parent = null;
+            
+        }
+        
+
+        // go.transform.SetParent(goWall.transform);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "PunchingBag")
+        {
+            activeAttachment = false;
+
+        }
+    }
+    private void OnTriggerEnter(Collider other1)
+    {
+        if (other1.tag=="PunchingBag")
+        {
+            activeAttachment = false;
+            
+        }
+        
+    }
+    private void OnTriggerExit(Collider other2)
+    {
+        if (other2.tag == "PunchingBag")
+        {
+            activeAttachment = true;
+        }
+    }
+}
