a286c8a8e21a351a29b721015f7376be38be217d
habeeb22 <habeeb22@users.noreply.github.com>
2019-06-12 10:15:22 +0400

    punching bag changed

---
+++ Assets/test/VivePointerFinishGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VivePointerFinishGame : MonoBehaviour
+{
+    public GameObject pointerP1;
+    public GameObject inputVrP1;
+    public GameObject exitScreen;
+    public Text player1ScoreText;
+    public Text player2ScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player1ScoreText.text = PlayerPrefs.GetInt("player1Score", 0).ToString();
+        player2ScoreText.text = PlayerPrefs.GetInt("player2Score", 0).ToString();
+        if (PlayerPrefs.GetInt("PointerP1", 1)==1)
+        {
+            exitScreen.SetActive(true);
+            pointerP1.SetActive(true);
+            inputVrP1.SetActive(true);
+        }
+        else
+        {
+            exitScreen.SetActive(false);
+            pointerP1.SetActive(false);
+            inputVrP1.SetActive(false);
+        }
+
+        
+    }
+}
---
+++ Assets/test/photonMyLocationObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class photonMyLocationObject : MonoBehaviourPun
+{
+    public GameObject stage1, stage2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!photonView.IsMine )
+        {
+            stage1.SetActive(false);
+
+        }
+        if (photonView.IsMine )
+        {
+            stage2.SetActive(false);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
